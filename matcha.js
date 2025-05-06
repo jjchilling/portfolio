b88fa0c5334d3848d57b6257ac3b3bb2236aa8d8
heesuch <juliehsuchung@gmail.com>
2025-05-05 22:22:44 -0400

    pleek

--- matcha.js
+++ matcha.js
@@ -2,9 +2,9 @@
 // import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 // import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.157.0/build/three.module.js";
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.157.0/examples/jsm/loaders/GLTFLoader.js";
-import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.157.0/examples/jsm/controls/OrbitControls.js";
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.157.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.157.0/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.157.0/examples/jsm/controls/OrbitControls.js';
 
 // import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.157.0/build/three.module.js";
 // import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.157.0/examples/jsm/loaders/GLTFLoader.js";
